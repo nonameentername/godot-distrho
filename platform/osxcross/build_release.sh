d1aa1d87f91b1c56fb4101955ca0c04477c42502
werner mendizabal <nonameentername@gmail.com>
2026-01-11 21:24:23 -0600

    Update macos build to work with godot v4.6

--- platform/osxcross/build_release.sh
+++ platform/osxcross/build_release.sh
@@ -17,7 +17,7 @@ cd $dir/modules/godot
 
 for ARCH in x86_64 arm64; do
     scons platform=macos arch=$ARCH library_type=static_library verbose=yes \
-        osxcross_sdk=$OSXCROSS_TARGET vulkan_sdk_path=/MoltenVK/MoltenVK/static/MoltenVK.xcframework
+        osxcross_sdk=$OSXCROSS_TARGET vulkan_sdk_path=/MoltenVK/MoltenVK/static/MoltenVK.xcframework metal=no
 done
 
 $dir/scripts/lipo-dir.py  \
@@ -46,7 +46,7 @@ done
 $dir/scripts/lipo-dir.py  \
     $dir/addons/distrho/bin/osxcross-arm64/release \
     $dir/addons/distrho/bin/osxcross-x86_64/release \
-    $dir/addons/distrho/bin/osxcross/release 
+    $dir/addons/distrho/bin/macos/release 
 
 # build godot-distrho (gdextension)
 
@@ -66,6 +66,6 @@ done
 $dir/scripts/lipo-dir.py  \
     $dir/addons/distrho/bin/osxcross-arm64/release \
     $dir/addons/distrho/bin/osxcross-x86_64/release \
-    $dir/addons/distrho/bin/osxcross/release 
+    $dir/addons/distrho/bin/macos/release 
 
 zsign -a $dir/addons/distrho/bin/macos/macos.framework/libdistrhogodot.macos.template_release
